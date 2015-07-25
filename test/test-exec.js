e2a2e5cdccdf50fa5ec80a56473ef3d2c0ade6f0
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-07-25 13:34:03 -0300

    fixed exec params

--- test/test-exec.js
+++ test/test-exec.js
@@ -6,8 +6,8 @@ var assert = require("assert");
 var Exec = require("../");
 
 var options = {
-  cmd: "",
-  args: [],
+  cmd: "node",
+  args: ["$script$","$param$-test"],
   opts: {}
 };
 
@@ -24,6 +24,19 @@ describe("Exec",function() {
   it("constructor ok",function() {
     var exec = new Exec(options);
     assert.ok(exec instanceof Exec);
+  });
+  
+  it("exec",function(done) {
+    var exec = new Exec(options);
+    exec.exec({ 
+      script: __dirname + "/sample/sample.js", 
+      param: "test-param"
+    },function(error,stdout,stderr) {
+      assert.strictEqual(error,null);
+      assert.strictEqual(stdout,"test-param-test");
+      assert.strictEqual(stderr,"");
+      done();
+    });
   });
   
 });
